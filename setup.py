7c655576e9406b4f21f91013b4b795e92fcb0e92
Win11BSOD <117566154+Win11BSOD@users.noreply.github.com>
2022-12-27 13:00:50 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='Downloader_plus',
+    version='0.0.3',
+    packages=['downloader_plus'],
+    url='https://space.bilibili.com/1933399514?spm_id_from=333.788.0.0',
+    author='Guo Minghao',
+    author_email='qu11182021@outlook.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)
